3623e643e5741f22ffd3534a52ff7aed5d2b2480
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-16 12:29:54 +0530

    Randomly return "rock", "paper", or "scissors"
    getRandomIntInclusive is useful to get a random integer between two
    values (inclusive) at both minimum and maximum. (mdn web docs)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+}
+
+function getComputerChoice() {
+  let computerChoice;
+  const value = getRandomIntInclusive(0, 2);
+  switch (value) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
